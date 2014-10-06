1056645e4364f463040bdcbeb35843ef012aa1a8
Dzmitry Kislo <Dzmitry_Kislo@epam.com>
2014-10-06 11:54:53 +0300

    second commit

---
+++ 01.Backend-Classloading/Functionality/src/main/java/com/epam/sample/JarSample.java
@@ -0,0 +1,10 @@
+package com.epam.sample;
+
+/**
+ * Created by dima on 3.10.14.
+ */
+public interface JarSample {
+    public static StringBuffer counter = new StringBuffer("");
+    int increment();
+    String toString();
+}
